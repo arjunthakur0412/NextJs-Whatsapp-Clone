aa2983134b5d75949c6b8e99dbbc9ce5a43f5936
Arjun Thakur <arjun.thakur@netprotector.net>
2024-05-16 14:28:40 +0530

    added upload photo feature

--- client/src/pages/_app.jsx
+++ client/src/pages/_app.jsx
@@ -1,5 +1,16 @@
-import "@/styles/globals.css";
+import { StateProvider } from '@/context/StateContext';
+import reducer, { initalState } from '@/context/StateReducers';
+import '@/styles/globals.css';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+	return (
+		<StateProvider initalState={initalState} reducer={reducer}>
+			<Head>
+				<title>Whatsapp</title>
+				<link rel='shortcut icon' href='/favicon.png' />
+			</Head>
+			<Component {...pageProps} />
+		</StateProvider>
+	);
 }
